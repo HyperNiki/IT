53ce978a13482690ff755296de587777c68c21ff
n.ivanov <n.ivanov@3l.ru>
2023-03-04 12:50:16 +0300

    Initial commit

---
+++ IT_DBG_1.0.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include <string.h>
+
+
+int main()
+{
+    const uint LED_PIN = 25;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT); 
+
+    stdio_init_all();
+    char str[80];
+    char UserInput;
+    //sleep_ms(5000);
+    int n = 0;
+    while(1)
+    {
+        while ((UserInput != '\n') && (n < 80))
+        {
+            while( !stdio_usb_connected() ){
+                printf("waiting for usb connection\n");
+                sleep_ms(100);
+            }
+            UserInput = getchar_timeout_us(10000);
+            str[n] = UserInput;
+            printf("n = %d \r\n", n);
+            printf("%c\r\n", UserInput);
+            n++;
+        }
+        printf("%s", str);
+        UserInput = '\0';
+        n = 0;
+        if (!strncmp (str, "LED 1",5))
+        {
+            gpio_put(25, 1);
+            printf("Led ON \r\n");
+        } else if (!strncmp (str, "LED 0",5))
+        {
+            gpio_put(25, 0);
+            printf("Led OFF \r\n");
+        } else 
+        printf("Invalid Input \r\n");
+    
+    }
+
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Interactive game
+
+An interactive game tool. Implemented a console that accepts commands via 
+usb. It is necessary for the game to work, debug and calibrate devices.
+Development takes place on rp2040
+
+## Devices
+
+* LED
+
+## Installation
+
+Coming soon...
+
+## Usage
+
+Move the generated `IT_DBG_1.0' to file system rp2040 in bootloader.
+
+### Commands
+
+Coming soon...
+
+## Screenshots
+
+Coming soon...
