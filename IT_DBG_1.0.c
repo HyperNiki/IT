e3893173f130af70ec46cc5f7ea8d97d06ea87bb
HyperNiki <giperdrakon@gmail.com>
2023-03-11 12:02:01 +0300

    Add code for driver motor + console: update reading and command for motor

--- IT_DBG_1.0.c
+++ IT_DBG_1.0.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
 #include <string.h>
 #include "motor.h"
+#include <stdlib.h>
+
+unsigned char mygetchar();
+void Clear_r_n(char str[], int Len);
+void Clear_Space(char str[]);
+char CorrectSymbol(char elem);
 
 int main()
 {
@@ -12,45 +17,185 @@ int main()
     stdio_init_all();
     char str[80];
     char UserInput;
-    //sleep_ms(5000);
+    char * pEnd;
+    char * pEndx2;   
     int n = 0;
     struct motor MotorA;
+    struct motor MotorB;
 
-    MotorA.pin1 = 20;
-    MotorA.pin1 = 21;
-    MotorA.pin1 = 22;
-    MotorA.pin1 = 23;
+    MotorA.dir = 20;
+    MotorA.pin = 21;
+
+    MotorB.dir = 22;
+    MotorB.pin = 23;
 
     while(1)
     {
-        while ((UserInput != '\n') && (n < 80))
+        while ((UserInput != '\n') && (n < 78))
         {
-            while( !stdio_usb_connected() ){
-                printf("waiting for usb connection\n");
-                sleep_ms(100);
-            }
-            UserInput = getchar_timeout_us(10000);
+            UserInput = mygetchar();
             str[n] = UserInput;
-            printf("n = %d \r\n", n);
-            printf("%c\r\n", UserInput);
             n++;
         }
-        printf("%s", str);
+        Clear_r_n(str, strlen(str));
+        Clear_Space(str);
         UserInput = '\0';
         n = 0;
-        if (!strncmp (str, "LED 1",5))
+        
+        for (int IndexForCorrect = 0; IndexForCorrect < (strchr((char*)&str,' ') - str); IndexForCorrect++)
+		{
+		    str[IndexForCorrect] = CorrectSymbol(str[IndexForCorrect]);
+		}
+        
+        if (!strncmp (str, "led 1",5))
         {
             gpio_put(25, 1);
             printf("Led ON \r\n");
-        } else if (!strncmp (str, "LED 0",5))
+        } else if (!strncmp (str, "led 0",5))
         {
             gpio_put(25, 0);
             printf("Led OFF \r\n");
-        } else 
+        } else if (!strncmp (str, "motor ",6 ))
+        {
+		  	if ((strtol(str + 6, &pEnd ,10) != '\0' || (!strncmp(str+6,"0,",2))) &&
+		  	(strtol(strchr(pEnd,',')+1,&pEndx2,10) != '\0' || (!strcmp(strchr(pEnd,',')+1,"0"))))
+		  	{
+		  	    uint16_t motor = strtol(str+6, &pEnd,10);
+			    uint16_t value = strtol(strchr(pEnd,',')+1,&pEndx2,10);
+                if (motor)
+                DriveMotor(MotorA, value, 1);
+                else 
+                DriveMotor(MotorB, value, 1);
+		  	  	printf("Motor %d drive %d\r\n", motor, value);
+		  	} else {
+		  	  printf("No correct value\r\n");
+		  	}
+        } else
         printf("Invalid Input \r\n");
     
     }
 
 
     return 0;
+}
+
+// Function for get char without error
+unsigned char mygetchar() {
+	int c;
+	while ( (c = getchar_timeout_us(0)) < 0); 
+	return (unsigned char)c;
+};
+
+void Clear_r_n(char str[], int Len)
+{
+	int Index;
+	int Flag = 0;
+	for (Index = 0; Index < Len; Index++)
+	{
+		if (Flag == 0)
+		{
+			if ((str[Index] == '\n') || (str[Index] == '\r'))
+			{
+				str[Index] = '\0';
+				Flag = 1;
+			}
+		} else {
+			str[Index] = '\0';
+		}
+	}
+}
+
+void Clear_Space(char str[])
+{
+	while (str[strlen(str) - 1] == ' ')
+	{
+		str[strlen(str) - 1] = '\0';
+	}
+}
+
+char CorrectSymbol(char elem)
+{
+	switch(elem)
+	{
+	case 'A':
+		return 'a';
+		break;
+	case 'B':
+		return 'b';
+		break;
+	case 'C':
+		return 'c';
+		break;
+	case 'D':
+		return 'd';
+		break;
+	case 'E':
+		return 'e';
+		break;
+	case 'F':
+		return 'f';
+		break;
+	case 'G':
+		return 'g';
+		break;
+	case 'H':
+		return 'h';
+		break;
+	case 'I':
+		return 'i';
+		break;
+	case 'J':
+		return 'j';
+		break;
+	case 'K':
+		return 'k';
+		break;
+	case 'L':
+		return 'l';
+		break;
+	case 'M':
+		return 'm';
+		break;
+	case 'N':
+		return 'n';
+		break;
+	case 'O':
+		return 'o';
+		break;
+	case 'P':
+		return 'p';
+		break;
+	case 'Q':
+		return 'q';
+		break;
+	case 'R':
+		return 'r';
+		break;
+	case 'S':
+		return 's';
+		break;
+	case 'T':
+		return 't';
+		break;
+	case 'U':
+		return 'u';
+		break;
+	case 'V':
+		return 'v';
+		break;
+	case 'W':
+		return 'w';
+		break;
+	case 'X':
+		return 'x';
+		break;
+	case 'Y':
+		return 'y';
+		break;
+	case 'Z':
+		return 'z';
+		break;
+	}
+
+	return elem;
 }
--- motor/motor.h
+++ motor/motor.h
@@ -9,14 +9,11 @@
 #define stepsperrev 4096
 
 struct motor {
-    uint   pin1;        /* state machine number */
-    uint   pin2;        /* state machine number */
-    uint   pin3;        /* state machine number */
-    uint   pin4;        /* state machine number */
+    uint   dir;        /* state machine number */
+    uint   pin;        /* state machine number */
 };
 
-void stepper_step_angle (float angle, int direction, int rpm, struct motor *motor);
-void stepper_set_rpm (int rpm);  // Set rpm--> max 13, min 1,,,  went to 14 rev/min
-void stepper_half_drive (int step, struct motor *motor);
+void DriveMotor(struct motor motor,int Steps, int Direction);
+void DriveTwoMotors(struct motor motor1,int Direction1, struct motor motor2, int Direction2, int Steps);
 
 #endif /* MOTOR */
